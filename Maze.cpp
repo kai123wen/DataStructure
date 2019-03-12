2f5fb7d05b81b132f91f1b2d52a7f41be3f3ccb4
kai123wen <1819306561@qq.com>
2019-03-12 13:40:04 +0800

    修改

--- Maze.cpp
+++ Maze.cpp
@@ -19,11 +19,12 @@ typedef struct{
 	int top;//栈顶指针
 } Stack;
 
-bool findPath(int inX,int inY,int outX,int outY);
+bool findPath(int inX,int inY,int outX,int outY,int maze[][7]);
+
 
 
 int main(){
-	
+
 	//存储迷宫的二维数组,1表示为有障碍不可通过,0表示无障碍可以通过
 	int maze[row+2][column+2] = {1,1,1,1,1,1,1,
 	                             1,0,0,0,1,0,1,
@@ -32,71 +33,72 @@ int main(){
                  				 1,0,1,0,1,0,1,
                     			 1,0,0,0,0,0,1,
                 				 1,1,1,1,1,1,1};
-    
-	findPath(1,1,5,5);
+
+	findPath(1,1,5,5,maze);
 	return 0;
 }
 //findPath
-bool findPath(int inX,int inY,int outX,int outY){
+bool findPath(int inX,int inY,int outX,int outY,int maze[][7]){
 	Stack stack;
 	int x,y,di;
 	int find = 0;//定义找到的变量
-	stack->top = -1;
-	stack->top++;
-	stack.data[stack->top].i = intX;
-	stack.data[stack->top].j = intY;
-	stack.data[stack->top].di = -1;
-	maze[intX][intY] = -1; //避免重复，当一个方块进栈时，将迷宫值改为-1
+	stack.top = -1;
+	stack.top++;
+	stack.data[stack.top].i = inX;
+	stack.data[stack.top].j = inY;
+	stack.data[stack.top].di = -1;
+	maze[inX][inY] = -1; //避免重复，当一个方块进栈时，将迷宫值改为-1
 	while(stack.top > -1){// 栈不为空
-	
+
 	    //取出栈顶元素
 		x = stack.data[stack.top].i;
 		y = stack.data[stack.top].j;
 		di = stack.data[stack.top].di;
-		
+
 		if(x == outX && y == outY){//到了终点
 			cout<<"路径为"<<endl;
 			while(stack.top != -1){
-				cout<<stack.data[stack.top]<<"->";
+				cout<<stack.data[stack.top].i<<" "<<stack.data[stack.top].j<<endl;
 				stack.top--;
 			}
 			return true;
 		}
-		while(di<4 && find == 0){//继续找下去
+		find = 0;
+		while(di<4 && find == 0)
+            {//继续找下去
 			di++;
 			switch(di){
-				case 0:
-				x = x+1;y = y;
+            case 0:
+				x = stack.data[stack.top].i+1;y = stack.data[stack.top].j;
 				break;
-				case 1:
-				x = x;y = y+1;
+            case 1:
+                x = stack.data[stack.top].i;y = stack.data[stack.top].j+1;
 				break;
-				case 2:
-				x = x - 1;
-				y = y;
+            case 2:
+				x = stack.data[stack.top].i - 1;y = stack.data[stack.top].j;
 				break;
-				case 3:
-				x=x;
-				y = y-1;
+            case 3:
+				x=stack.data[stack.top].i;y = stack.data[stack.top].j-1;
+				break;
 			}
-			if(maze[x][y] ==0){
+			if(maze[x][y]==0){
 				find = 1;//找到下一步方块
 			}
-			
 		}
-		
-		if(find == 1){    
+
+		if(find == 1){
 			stack.data[stack.top].di = di;
 			stack.top++;
 			stack.data[stack.top].i = x;
 			stack.data[stack.top].j = y;
+			cout<<stack.data[stack.top].i<<" "<<stack.data[stack.top].j<<"进栈"<<endl;
 			stack.data[stack.top].di = -1;
 			maze[x][y] = -1;//避免重复
 		}else{//没有路径可走，则退栈
 			maze[stack.data[stack.top].i][stack.data[stack.top].j] = 0;
 			stack.top--;
 		}
-		
+
 	}
 	return false;
 }
